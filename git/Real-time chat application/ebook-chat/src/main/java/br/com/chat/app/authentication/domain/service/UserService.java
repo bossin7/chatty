98cfc3865b4f00fcfd153a11005f91dac00de4a6
bossin7 <bapesapplik@yahoo.com>
2018-02-15 11:23:38 +0100

    Final commit
    The final commit before the release

---
+++ git/Real-time chat application/ebook-chat/src/main/java/br/com/chat/app/authentication/domain/service/UserService.java
@@ -0,0 +1,8 @@
+package br.com.chat.app.authentication.domain.service;
+
+import br.com.jorgeacetozi.ebookChat.authentication.domain.model.User;
+
+public interface UserService {
+
+	User createUser(User user);
+}
